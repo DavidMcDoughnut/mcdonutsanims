15088856d740b6cf4b96a490b6f3bd5e9d4adccb
David McDonough <dm.mcdonough@gmail.com>
2025-02-18 22:57:33 -0500

    Update webpack config and add json-loader

--- next.config.js
+++ next.config.js
@@ -5,10 +5,19 @@ const nextConfig = {
     unoptimized: true,
   },
   webpack: (config) => {
+    // Add rule for JSON files
     config.module.rules.push({
-      test: /\.(json|webm)$/,
+      test: /\.json$/,
+      type: 'javascript/auto',
+      use: ['json-loader']
+    });
+    
+    // Add rule for media files
+    config.module.rules.push({
+      test: /\.(webm|mp4|png|jpg|jpeg|gif|svg)$/i,
       type: 'asset/resource'
     });
+
     return config;
   }
 };
