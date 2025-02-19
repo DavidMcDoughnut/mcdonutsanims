44ba6ef46a2d990354e90234aa70d7274bb3bd13
David McDonough <dm.mcdonough@gmail.com>
2025-02-18 22:46:15 -0500

    Add Vercel and Next.js configurations for static file handling

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(json|webm)$/,
+      type: 'asset/resource'
+    });
+    return config;
+  }
+};
 
 module.exports = nextConfig; 
